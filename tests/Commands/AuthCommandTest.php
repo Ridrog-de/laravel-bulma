5e74ec3305d5ef06ec88d34d188d9d1c59ea9211
Ridrog <michael@ridrog.de>
2017-06-27 22:45:42 +0200

    fixed problem with classname

--- tests/Commands/AuthCommandTest.php
+++ tests/Commands/AuthCommandTest.php
@@ -5,7 +5,7 @@ namespace Ridrog\Bulma\Test\Commands;
 use Illuminate\Support\Facades\Artisan;
 use Ridrog\Bulma\Test\TestCase as TestCase;
 
-class PresetCommandTest extends TestCase
+class AuthCommandTest extends TestCase
 {
     /** @test */
     public function it_can_run_the_bulma_auth_command()
