10643bfaaae1aa40c7d94191c2ecc25c6d688772
Ridrog <michael@ridrog.de>
2017-06-27 22:46:03 +0200

    removed unused 'use' statement

--- src/Bulma.php
+++ src/Bulma.php
@@ -2,9 +2,16 @@
 
 namespace Ridrog\Bulma;
 
-use Illuminate\Filesystem\Filesystem;
-
 class Bulma
 {
+    public $folders;
 
+    public $directories;
+
+    public $views;
+
+    public function __construct()
+    {
+        $this->directories = config('bulma.directories');
+    }
 }
